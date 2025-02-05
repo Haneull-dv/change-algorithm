86c443d54bff9e01337edf09313baa94db826f39
devhaneulkim <haneul.kim1123@gmail.com>
2025-02-05 16:00:15 +0900

    거스름돈 알고리즘 완성! 2025-02-05 16:00

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=["POST", "GET"] )
 def paid():
     print("💰거스름돈 계산기💰")
+
     if request.method == "POST" :
         print("😊POST 접근😊")       
         total = request.form.get('total')
@@ -16,12 +16,10 @@ def paid():
         print("amount: ", amount)
         
         if amount < 0 :
-            error="🔥금액을 더 지불하지 않을래?🔥"
+            error ="🔥금액을 더 지불하지 않을래?🔥"
             return render_template("index.html", error=error)
-        else:
-
-
         
+        else:
             COIN_500 = 500
             COIN_100 = 100
             COIN_50 = 50
@@ -34,17 +32,11 @@ def paid():
             coin50 = coin100_nmg // COIN_50
             coin50_nmg = coin100_nmg % COIN_50
             coin10 = coin50_nmg // COIN_10     
-
-            return render_template('index.html', total=total, paid=paid, coin500=coin500, coin100=coin100, coin50=coin50, coin10=coin10, amount=amount)
+            return render_template('index.html', 
+                                   total=total, paid=paid, coin500=coin500, coin100=coin100, coin50=coin50, coin10=coin10, amount=amount)
 
     else:
          print("😊GET 접근😊")
          return render_template('index.html')
-
-
-
-
-   
-     
     
 if __name__ == "__main__": app.run(debug=True)
